607fdbc01b4fda2b329173234e0eaf2e63a1da07
Fan-tome14 <96857821+Fan-tome14@users.noreply.github.com>
2025-05-05 17:25:05 -0400

    scene decollage

---
+++ Assets/script/debut/portal/portalscenechanged.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portalsceneChanger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Vérifie si la main touche le bouton
+        {
+            LoadMainScene();
+        }
+    }
+
+    public void LoadMainScene()
+    {
+        Debug.Log("Chargement de la sc�ne main...");
+        SceneManager.LoadScene("decollage"); // Charge la scéne "decolage"
+    }
+}
